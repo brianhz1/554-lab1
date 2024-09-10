4ea699737c058cfb339e60b2e88fadda6ef8a0e3
brianhz1 <131827063+brianhz1@users.noreply.github.com>
2024-09-10 16:51:02 -0500

    add initial files

---
+++ driver.v
@@ -0,0 +1,33 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    
+// Design Name: 
+// Module Name:    driver 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module driver(
+    input clk,
+    input rst,
+    input [1:0] br_cfg,
+    output iocs,
+    output iorw,
+    input rda,
+    input tbr,
+    output [1:0] ioaddr,
+    inout [7:0] databus
+    );
+
+
+endmodule
---
+++ spart.v
@@ -0,0 +1,34 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:   
+// Design Name: 
+// Module Name:    spart 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module spart(
+    input clk,
+    input rst,
+    input iocs,
+    input iorw,
+    output rda,
+    output tbr,
+    input [1:0] ioaddr,
+    inout [7:0] databus,
+    output txd,
+    input rxd
+    );
+
+
+endmodule
