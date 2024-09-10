1e5826a1b9caee719588efe5d5e7f4ae38b8f3af
brianhz1 <131827063+brianhz1@users.noreply.github.com>
2024-09-10 17:53:58 -0500

    editted

--- baud_gen.sv
+++ baud_gen.sv
@@ -1,23 +1,23 @@
 module baud_gen
 #(  
-    parameter clk = 50000000,    // 50 Mhz
+    parameter clk_rate = 50000000,    // 50 Mhz
     parameter baud_rate = 19200; // bit per second
 )
 (
-    input i_clk,
-    input i_init,
-    output o_baud_clk
+    input clk,
+    input init,
+    output baud_clk
 );
 
-    logic [$clog2(clk/baud_rate)-1:0] counter;
+    logic [$clog2(clk_rate/baud_rate)-1:0] counter;
 
-    always_ff @(posedge i_clk) begin
-        if (i_init)
+    always_ff @(posedge clk) begin
+        if (init)
             counter <= 0;
         else
             counter <= counter + 1;
     end
 
-    assign o_baud_clk = &counter;
+    assign baud_clk = &counter;
 
 endmodule
