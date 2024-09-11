72282831c841f12013eb4e7dce00afb204ff827e
brianhz1 <131827063+brianhz1@users.noreply.github.com>
2024-09-11 14:26:05 -0500

    added baud rate selection

--- baud_gen.sv
+++ baud_gen.sv
@@ -1,23 +1,31 @@
 module baud_gen
-#(  
-    parameter clk_rate = 50000000,    // 50 Mhz
-    parameter baud_rate = 19200; // bit per second
-)
 (
     input clk,
     input init,
-    output baud_clk
+    input [1:0] br_cfg,
+    output enable // 16x baud rate
 );
 
-    logic [$clog2(clk_rate/baud_rate)-1:0] counter;
+    logic [11:0] counter;   // down counter
+    logic [11:0] load;  // value to load into down counter
+
+    always_comb begin
+        case (br_cfg)
+            2'b00: load = 11'd300;
+            2'b01: load = 11'd600;
+            2'b10: load = 11'd1200;
+            // 2'b11
+            default: load = 11'd2400;
+        endcase
+    end
 
     always_ff @(posedge clk) begin
         if (init)
-            counter <= 0;
+            counter <= ;
         else
-            counter <= counter + 1;
+            counter <= counter - 1;
     end
 
-    assign baud_clk = &counter;
+    assign enable = counter == 0;
 
 endmodule
