dce3661f57bfa793f89e9333ed4f9b1d4a6bcc03
brianhz1 <131827063+brianhz1@users.noreply.github.com>
2024-09-12 12:47:17 -0500

    added divisor buffer

--- baud_gen.sv
+++ baud_gen.sv
@@ -1,27 +1,34 @@
 module baud_gen
 (
     input clk,
+    input rst_n,
     input init,
     input [1:0] br_cfg,
     output enable // 16x selected baud rate
 );
 
-    logic [11:0] counter;   // down counter
-    logic [11:0] load;  // value to load into down counter
+    logic [9:0] counter;   // down counter
+    logic [9:0] load;  // value to load into divisor buffer
+    logic [9:0] divisor_buffer; // value to load into down counter
 
     always_comb begin
         case (br_cfg)
-            2'b00: load = 11'd651;  // 4800 baud * 16
-            2'b01: load = 11'd326;  // 9600 baud * 16
-            2'b10: load = 11'd163;  // 19200 baud * 16
+            2'b00: load = 9'd651;  // 4800 baud * 16
+            2'b01: load = 9'd326;  // 9600 baud * 16
+            2'b10: load = 9'd163;  // 19200 baud * 16
             // 2'b11
-            default: load = 11'd81; // 38400 baud * 16
+            default: load = 9'd81; // 38400 baud * 16
         endcase
+    end
+
+    always_ff @(posedge clk, negedge rst_n) begin
+        if (!rst_n)
+            divisor_buffer <= load;
     end
 
     always_ff @(posedge clk) begin
         if (init)
-            counter <= load;
+            counter <= divisor_buffer;
         else
             counter <= counter - 1;
     end
