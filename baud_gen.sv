073c16f5119dc705d80f5af745054510221bd0cf
brianhz1 <131827063+brianhz1@users.noreply.github.com>
2024-09-12 12:30:20 -0500

    fixed baud load values

--- baud_gen.sv
+++ baud_gen.sv
@@ -3,7 +3,7 @@ module baud_gen
     input clk,
     input init,
     input [1:0] br_cfg,
-    output enable // 16x baud rate
+    output enable // 16x selected baud rate
 );
 
     logic [11:0] counter;   // down counter
@@ -11,17 +11,17 @@ module baud_gen
 
     always_comb begin
         case (br_cfg)
-            2'b00: load = 11'd300;
-            2'b01: load = 11'd600;
-            2'b10: load = 11'd1200;
+            2'b00: load = 11'd651;  // 4800 baud * 16
+            2'b01: load = 11'd326;  // 9600 baud * 16
+            2'b10: load = 11'd163;  // 19200 baud * 16
             // 2'b11
-            default: load = 11'd2400;
+            default: load = 11'd81; // 38400 baud * 16
         endcase
     end
 
     always_ff @(posedge clk) begin
         if (init)
-            counter <= ;
+            counter <= load;
         else
             counter <= counter - 1;
     end
